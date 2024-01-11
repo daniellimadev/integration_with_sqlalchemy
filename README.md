d00c89380c563433dbfe2fdbc5f146dd212ce6d1
Daniel Lima <pdaniel643@gmail.com>
2024-01-11 17:24:20 -0300

    Integrating Python with SQLAlchemy
    Signed-off-by: Daniel Lima <pdaniel643@gmail.com>

---
+++ README.md
@@ -0,0 +1,29 @@
+# Integration with SQLAlchemy
+
+## Technologies used
+
+* **Language:** Python
+* **Versioning:** Git/GitHub
+* **IDE:** Visual Studio Code
+* **Data Base:** SQLite, Mongo
+* **Framework:** SQLAlchemy is ORM
+
+---
+
+## Description
+
+Work with the leading ORM integration library
+DBMSs with Python – SQLAlchemy. Furthermore, we cannot
+leave out NoSQL databases. Therefore,
+we will use Pymongo.
+
+<br>
+
+<h3>Author</h3>
+
+<a href="https://www.linkedin.com/in/danielpereiralima/">
+ <img style="border-radius: 50%;" src="https://avatars.githubusercontent.com/u/96916005?v=4" width="100px;" alt=""/>
+
+Made by Daniel Pereira Lima 👋🏽 Contact!
+
+[![Linkedin Badge](https://img.shields.io/badge/-Daniel-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/danielpereiralima/)](https://www.linkedin.com/in/danielpereiralima/)
---
+++ integrationWithSQL/sqlAlchemyApplication.py
@@ -0,0 +1,36 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user_account"
+    # attribute
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    
+    
+    address = relationship(
+        "Address", back_populates="user", cascade="all, delete-orphan"
+    )
+    
+class Address(Base):
+    __tablename__ = "address"
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String(30), nullable=False)
+    user_id = Column(Integer, ForeignKey("user_account.id"), nullable=False)
+
+    user = relationship("User", back_populates="address")
+    
+    def __repr__(self):
+        return f"Address (id={self.id}, email={self.email_address})"
+    
+    
+print(User.__tablename__)
+print(Address.__tablename__)
